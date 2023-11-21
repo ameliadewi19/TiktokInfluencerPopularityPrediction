09841010d29834b967d01cad3b196cf751137f7a
adindafaayza <afm243.sch@gmail.com>
2023-11-21 14:53:35 +0700

    Edit Kampanye

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from bson import ObjectId
 from pymongo import MongoClient
 from functools import wraps, update_wrapper
 from datetime import datetime
+from sync_video import sync_video
 
 app = Flask(__name__)
 
@@ -93,9 +94,11 @@ def kampanye():
     for campaign_data in campaigns_data:
         campaign_name = campaign_data.get('namaCampaign', '')
         video_count = len(campaign_data.get('video', []))
+        id = campaign_data.get('_id', '')
         campaigns.append({
             'namaCampaign': campaign_name,
-            'videoCount': video_count
+            'videoCount': video_count,
+            'id': id,
         })
 
     return render_template("kampanye.html", campaigns=campaigns)
@@ -121,5 +124,129 @@ def detail_kampanye():
     else:
         # Handle kasus ketika campaign tidak ditemukan
         return render_template("error.html", message="Campaign not found")
+
+@app.route('/save_posts', methods=['POST'])
+@nocache
+def save_posts():
+    campaign_id = request.args.get('campaign_id')
+
+    try:
+        campaign_id = ObjectId(campaign_id)
+    except Exception as e:
+        print(f"Error converting campaign_id to ObjectId: {e}")
+        return render_template("error.html", message="Invalid Campaign ID")
+
+    if request.method == 'POST':
+        video_urls = request.form.getlist('videoURLs[]')
+
+        try:
+            collection_campaign.update_one(
+                {"_id": campaign_id},
+                {"$set": {"video": [{"video_url": url} for url in video_urls]}}
+            )
+
+            return redirect(url_for('detail_kampanye', campaign_id=campaign_id))
+
+        except Exception as e:
+            print(f"Error updating campaign: {e}")
+            return render_template("error.html", message="Error updating campaign")
+
+
+
+@app.route('/add_campaign', methods=['POST'])
+def add_campaign():
+    if request.method == 'POST':
+        name = request.form.get('addCampaignName')
+
+        # Simpan ke database MongoDB
+        collection_campaign.insert_one({'namaCampaign': name})  # Tambahkan videoCount sesuai kebutuhan
+
+    return redirect(url_for('kampanye'))
+
+@app.route("/edit_kampanye", methods=["POST"])
+@nocache
+def edit_kampanye():
+    campaign_id = request.args.get('campaign_id')
+
+    try:
+        campaign_id = ObjectId(campaign_id)
+    except Exception as e:
+        print(f"Error converting campaign_id to ObjectId: {e}")
+        return render_template("error.html", message="Invalid Campaign ID")
+
+    if request.method == "POST":
+        edited_campaign_name = request.form.get('edited_campaign_name')
+
+        try:
+            campaign_id = ObjectId(campaign_id)
+        except Exception as e:
+            print(f"Error converting campaign_id to ObjectId: {e}")
+            return render_template("error.html", message="Invalid Campaign ID")
+
+        try:
+            collection_campaign.update_one({"_id": campaign_id}, {"$set": {"namaCampaign": edited_campaign_name}})
+            return redirect(url_for('kampanye'))
+        except Exception as e:
+            print(f"Error editing campaign")
+            return redirect(url_for('kampanye'))
+
+@app.route("/edit_video", methods=["POST"])
+@nocache
+def edit_video():
+    campaign_id = request.args.get('campaign_id')
+    video_url= request.args.get('video_url')
+
+    try:
+        campaign_id = ObjectId(campaign_id)
+    except Exception as e:
+        # Handle kesalahan jika campaign_id tidak valid
+        print(f"Error converting campaign_id to ObjectId: {e}")
+        return render_template("error.html", message="Invalid Campaign ID")
+
+    if request.method == "POST":
+        edited_video_url = request.form.get('edited_video_url')
+
+        try:
+            collection_campaign.update_one(
+                {'_id': campaign_id, 'video.video_url': video_url},
+                {'$set': {'video.$.video_url': edited_video_url}}
+            )
+            # Redirect to the page displaying the campaigns
+            return redirect(url_for('detail_kampanye', campaign_id=campaign_id))
+        except Exception as e:
+            print(f"Error editing campaign: {e}")
+            return redirect(url_for('detail_kampanye', campaign_id=campaign_id))
+
+
+@app.route('/delete_kampanye', methods=['GET'])
+@nocache
+def delete_kampanye():
+    campaign_id = request.args.get('campaign_id')
+
+    try:
+        campaign_id = ObjectId(campaign_id)
+        collection_campaign.delete_one({'_id': campaign_id})
+        return redirect(url_for('kampanye'))
+    except Exception as e:
+        print(f"Error deleting campaign")
+        return redirect(url_for('kampanye'))
+
+@app.route('/delete_video', methods=['GET'])
+@nocache
+def delete_video():
+    campaign_id = request.args.get('campaign_id')
+    video_url = request.args.get('video_url')
+
+    try:
+        campaign_id = ObjectId(campaign_id)
+        collection_campaign.update_one(
+            {'_id': campaign_id},
+            {'$pull': {'video': {'video_url': video_url}}}
+        )
+        return redirect(url_for('detail_kampanye', campaign_id=campaign_id))
+    except Exception as e:
+        print(f"Error deleting video: {e}")
+        return redirect(url_for('detail_kampanye', campaign_id=campaign_id))
+
 if __name__ == '__main__':
     app.run(debug=True)
