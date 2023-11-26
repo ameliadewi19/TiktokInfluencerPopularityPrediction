8ffb768eaf7bbd8b0487d55c0999d93e46de873b
ameliadewi19 <ameliadewi1929@gmail.com>
2023-11-26 19:09:47 +0700

    updat login

--- app.py
+++ app.py
@@ -71,7 +71,9 @@ def login_process():
     username = request.form['username']
     password = request.form['password']
 
-    print("Tesss")
+    if not username or not password:
+        # If username or password is empty, redirect with an empty_fields parameter
+        return redirect(url_for('login', empty_fields=True))
 
     # Query the database for the user with the provided username
     user_data = collection_user.find_one({'username': username})
